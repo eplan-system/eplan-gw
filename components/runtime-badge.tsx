cdf3d736b0f07192f7196b4304d29deed71aa59f
ep.yito <ep.yito@gmail.com>
2026-03-31 08:48:28 +0900

    Rename runtime badge to online

--- components/runtime-badge.tsx
+++ components/runtime-badge.tsx
@@ -4,7 +4,7 @@ import { isFirebaseConfigured } from "@/lib/firebase";
 
 export function RuntimeBadge() {
   if (isFirebaseConfigured) {
-    return <span className="runtime-badge live">接続中</span>;
+    return <span className="runtime-badge live">オンライン</span>;
   }
 
   return <span className="runtime-badge demo">デモ</span>;
